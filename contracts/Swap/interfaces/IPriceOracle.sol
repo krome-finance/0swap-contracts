ac8da00393baf0442e0dfa2a99d28cf93e900abc
Titanium.steel <titanium@krome.finance>
2022-07-27 16:14:24 +0900

    fix invalid comment

--- contracts/Swap/interfaces/IPriceOracle.sol
+++ contracts/Swap/interfaces/IPriceOracle.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.8.0;
 
 interface IPriceOracle {
-    // returns 1e18 usd price of 1 unit amount in usd
     function getLatestPrice() external view returns (uint256);
     function getDecimals() external view returns (uint256);
 }
